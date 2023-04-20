66ec452786658985937383315df527098ff8d8e1
jjackson15 <102105912+jjackson15@users.noreply.github.com>
2023-04-20 15:18:03 -0400

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,30 @@
+# Change the content according to your package.
+# from directory.filename.py import classname
+from pycomputation.pycomputation import PyComputation
+
+__author__ = 'Group 4'
+__email__ = 'jennai.jackson@bison.howard.edu'
+__version__ = '0.1.0'
+
+# module level doc-string
+__doc__ = """
+PyCalc
+================
+
+Description
+-----------
+PyComputation is a Python package created to act as a calculator for users. This package will work to do all the functions that can be done by a scientific calculator. This way, they can easily solve equations using the package's functions rather than having to create their own.
+
+Example
+-------
+>>> # Import library
+>>> from pycomputation import PyComputation
+>>> # Initialize
+>>> model = PyComputation(" 2 3 4")
+>>> # Run the model
+>>> model.addition()
+
+References
+----------
+* https://towardsdatascience.com/a-step-by-step-guide-in-detecting-causal-relationships-using-bayesian-structure-learning-in-python-c20c6b31cee5
+"""
