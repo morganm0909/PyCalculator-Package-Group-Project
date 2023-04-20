ba2cacf27e32418f3c5e21f1d5c5844e1c1cc2ab
morganm0909 <104795413+morganm0909@users.noreply.github.com>
2023-04-20 15:45:39 -0400

    Update pycomputation.py
    code comments

--- pycomputation.py
+++ pycomputation.py
@@ -5,24 +5,30 @@ class PyComputation():
   def __init__(self, nums):
     # takes in string
     self.nums = nums
-
+  
+  #function takes in any amount of numbers and adds them (returns total)
   def add(self):
+    #converts string into list of strings separated by spaces
     num_list = self.nums.split(" ")
     total = 0
     for i in num_list:
       if i != ',':
         total += int(i)
     return total
-    
+  
+  #function takes in any amount of numbers subtracts them in order entered (returns total)
   def subtract(self):
+    #converts string into list of strings separated by spaces
     num_list = self.nums.split(" ")
     total = int(num_list[0])
     num_list.pop(0)
     for i in num_list: 
       total -= int(i)
     return total
-      
+  
+  #function takes in any amount of numbers and multiplies them (returns total)
   def multiply(self):
+    #converts string into list of strings separated by spaces
     num_list = self.nums.split(" ")
     total = int(num_list[0])
     num_list.pop(0)
@@ -30,7 +36,9 @@ class PyComputation():
       total = total * int(i)
     return total
   
+  #function takes in any amount of numbers and divides them in order entered (returns total)
   def divide(self):
+    #converts string into list of strings separated by spaces
     num_list = self.nums.split(" ")
     total = int(num_list[0])
     num_list.pop(0)
@@ -38,6 +46,7 @@ class PyComputation():
       total /= int(i)
     return total
   
+  #function takes in one perfect square value and returns its square root
   def square_root(self):
     target = int(self.nums)
     low = 1
@@ -54,13 +63,15 @@ class PyComputation():
     
 
   def exponent(self):
+    #converts string into list of strings separated by spaces
     num_list = self.nums.split(" ")
     total = int(num_list[0])
     num_list.pop(0)
     for i in num_list:
       total = total ** int(i)
     return total
-    
+  
+  #function takes in one perfect square value and returns its root
   def root(self):
     target = int(self.nums)
     for i in range(2, target):
@@ -71,21 +82,24 @@ class PyComputation():
           return i
     if total > target:
       print("This number does not have a root.")
-      
+  
+  #function takes in two values, first:the index root and second:the radicand
   def index_root(self):
+    #converts string into list of strings separated by spaces
     num_list = self.nums.split(" ")
     x = int(num_list[0]) 
     return int(num_list[1])**(1/x)
-    
+  
+  #function takes in one number and returns its absolution value
   def absolute_value(self):
     i = int(self.nums)
     if int(i) < 0:
         return int(i) * -1
     else:
         return int(i)
-        
+  
+  #function takes in two numbers and one x value in a^2 + b^2 = c^2 order format and returns x's numerical value
   def trig(self):
-    #enter numbers in a^2 + b^2 = c^2 order format
     num_list = self.nums.split(" ")
     if num_list[0] == 'x':
       a_squared = int(num_list[2])**2 - int(num_list[1])**2
